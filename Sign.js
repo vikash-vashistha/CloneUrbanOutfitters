62a9097d5e279db67dc2a98788c5a9e174178e52
mbnaveen <navinsmb@gmail.com>
2022-01-18 14:59:58 +0530

    sixth

--- Sign.js
+++ Sign.js
@@ -1,3 +1,9 @@
 document.getElementById("signb").addEventListener("click",function(){
     document.getElementById("popup").style.display="flex"
 })
+
+document.getElementById("sign_upb").addEventListener("click",function(event){
+event.preventDefault()
+document.getElementById("popup").style.display="none"
+document.getElementById("pops").style.display="flex"
+})
