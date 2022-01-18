d133cefb035ffdcb97f7adcd3afe3bc670443ebe
mbnaveen <navinsmb@gmail.com>
2022-01-18 15:27:25 +0530

    eight

--- Sign.js
+++ Sign.js
@@ -1,16 +1,21 @@
 document.getElementById("signb").addEventListener("click",function(){
     document.getElementById("popup").style.display="flex"
 })
-
 document.getElementById("sign_upb").addEventListener("click",function(event){
 event.preventDefault()
 document.getElementById("popup").style.display="none"
 document.getElementById("pops").style.display="flex"
 })
-
-
 document.getElementById("sign_b").addEventListener("click",function(event){
     event.preventDefault()
     document.getElementById("popup").style.display="flex"
     document.getElementById("pops").style.display="none"
     })
+document.getElementById("clo_divf").addEventListener("click",function(event){
+        event.preventDefault()
+        document.getElementById("popup").style.display="none"
+        })
+document.getElementById("clo_divs").addEventListener("click",function(event){
+        event.preventDefault()
+        document.getElementById("pops").style.display="none"
+        })
