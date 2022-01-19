ac2b712ca44d9532c370ff971e13a67b4b80f2c7
mbnaveen <navinsmb@gmail.com>
2022-01-19 20:22:07 +0530

    super g

--- Sign.js
+++ Sign.js
@@ -1,9 +1,27 @@
 
-let checr=JSON.parse(localStorage.getItem("checkiacc"))
-if(checr>0){
-    document.getElementById("myacc").textContent="My Account"
-}
 
+function check_user(){
+    let checr=JSON.parse(localStorage.getItem("checkiacc"))
+    if(checr>0){
+        document.getElementById("myaccdiv").style.display="flex"
+        console.log(checr)
+    }else{  
+        document.getElementById("myaccdiv").style.display="none"
+        console.log(checr)
+    }
+    if(checr==0||checr==null){
+        document.getElementById("signindiv").style.display="flex"
+        console.log(checr)
+    }else{  
+        document.getElementById("signindiv").style.display="none"
+        console.log(checr)
+    }
+}
+check_user()
+document.getElementById("myacc").addEventListener("click",function(){
+    localStorage.setItem("checkiacc",JSON.stringify(0))
+    check_user()
+})
 document.getElementById("signb").addEventListener("click",function(){
     document.getElementById("popup").style.display="flex"
 })
@@ -11,27 +29,39 @@ document.getElementById("sign_upb").addEventListener("click",function(event){
 event.preventDefault()
 document.getElementById("popup").style.display="none"
 document.getElementById("pops").style.display="flex"
+document.getElementById("inform").textContent=""
+document.getElementById("sigin").reset()
+document.getElementById("sigup").reset()
 })
 document.getElementById("sign_b").addEventListener("click",function(event){
     event.preventDefault()
-   
     document.getElementById("popup").style.display="flex"
     document.getElementById("pops").style.display="none"
+    document.getElementById("errf").textContent=""
+    document.getElementById("sigin").reset()
+    document.getElementById("sigup").reset()
     })
 document.getElementById("clo_divf").addEventListener("click",function(event){
         event.preventDefault()
         document.getElementById("popup").style.display="none"
-        
+        document.getElementById("errf").textContent=""
+        document.getElementById("inform").textContent=""
+        document.getElementById("inform").textContent=""
+        document.getElementById("sigin").reset()
+        document.getElementById("sigup").reset()
         })
 document.getElementById("clo_divs").addEventListener("click",function(event){
         event.preventDefault()
         document.getElementById("pops").style.display="none"
-        
+        document.getElementById("errf").textContent=""
+        document.getElementById("sigin").reset()
+        document.getElementById("sigup").reset()
         })
         var mainarr=JSON.parse(localStorage.getItem("mainsignup"))||[];
         var  c=0;
         document.getElementById("creacc").addEventListener("click",function(event){
           event.preventDefault();
+          let mainarr=JSON.parse(localStorage.getItem("mainsignup"))||[];
           let emails=document.getElementById("signup_mail").value;
           let passcode=document.getElementById("sineup_pass").value;
           if(emails==""||passcode==""){
@@ -45,18 +75,21 @@ for(let i=0;i<mainarr.length;i++){
 }
 if(c>0){
     c=0
-    alert("Account alresy exit")
-    
+    document.getElementById("errf").textContent="It looks like you already have an account with us!"
 }else{
     datas={
         mails:emails,
         passc:passcode
     }
     mainarr.push(datas);
+    localStorage.setItem("checkiacc",JSON.stringify(1))
+    check_user()
     localStorage.setItem("mainsignup",JSON.stringify(mainarr))
     document.getElementById("popup").style.display="none"
     document.getElementById("pops").style.display="none"
     document.getElementById("thirdp").style.display="flex"
+    document.getElementById("sigin").reset()
+    document.getElementById("sigup").reset()
 }
           }
         })
@@ -69,11 +102,11 @@ if(c>0){
             
         })
         var d=0;
-        var namec=[]
         var checkarr=JSON.parse(localStorage.getItem("mainsignup"))||[];
-
         document.getElementById("sign_inb").addEventListener("click",function(event){
             event.preventDefault();
+            let  checkarr=JSON.parse(localStorage.getItem("mainsignup"))||[];
+            console.log("yes",checkarr)
             let emailee=document.getElementById("ent_e").value;
             let pasee=document.getElementById("sign_p").value;
 
@@ -81,18 +114,23 @@ if(c>0){
                 alert("fields are empty")
             }else{
                 for(var j=0;j<checkarr.length;j++){
-                    if(checkarr[j].mails==emailee&&checkarr[j].passc==pasee){
+                    console.log(checkarr[j].mails,checkarr[j].passc)
+                    if((checkarr[j].mails==emailee)&&(checkarr[j].passc==pasee)){
                         d++
-
-                        document.getElementById("myacc").textContent="My Account"
-                        localStorage.setItem("checkiacc",JSON.stringify(d))
+                        console.log(checkarr[j].mails,checkarr[j].passc)
+localStorage.setItem("checkiacc",JSON.stringify(1)) 
+check_user()      
                     }
                 }
                 if(d>0){
                     document.getElementById("popup").style.display="none"
                     document.getElementById("pops").style.display="none"
+                    document.getElementById("sigin").reset()
+                    document.getElementById("sigup").reset()
+                    d=0
                 }else{
-                    alert("enter valid credentials")
+                    
+                    document.getElementById("inform").textContent="Please enter a valid email address and password."
                 }
             }
         })
